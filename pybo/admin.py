1eba3ae62150b979cef1394f26dc6c4cb354b01c
Donguk Shin <az9170@naver.com>
2023-06-24 23:16:25 +0900

    장고 관리자

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin): #검색기능 추가
+    search_fields = ['subject']
+
+admin.site.register(Question, QuestionAdmin)
